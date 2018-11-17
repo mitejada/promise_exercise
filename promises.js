6c539c5fca712216bf442b81b6c8121754c6d6f3
Michell Tejada <michelltejada@pursuit.com>
2018-11-16 21:01:23 -0500

    Working

--- promises.js
+++ promises.js
@@ -1,1 +1,43 @@
+function testNum (num) {
+  return new Promise ((resolve, reject) => {
+    resolve(num > 10);
+  }).then (bool => {
+    console.log(bool);
+  })
+}
 
+// testNum(8)
+
+
+function whisperShout (str) {
+  console.log(str);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(str.toUpperCase());
+    }, 3000);
+  }).then(upStr => {
+    console.log(upStr);
+    // return upStr
+  })
+}
+
+// whisperShout('hello')
+
+function waitFact(arr) {
+  let ind = 0;
+
+  let interval = setInterval(() => {
+    if(ind < arr.length){
+      new Promise((resolve, reject) => {
+        console.log(arr[ind]);
+        ind++;
+        resolve();
+      });
+    } else {
+    clearInterval(interval);
+    }
+  }, 3000)
+}
+
+waitFact([1,2,3,4])
